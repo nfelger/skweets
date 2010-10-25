f6aa59374a59ee007c73396eff69bed17ddfb109
Niko Felger <niko.felger@gmail.com>
2010-10-25 22:27:28 +0100

    commmmitt

---
+++ skweets.rb
@@ -0,0 +1,50 @@
+require 'rubygems'
+require 'twitter'
+require 'htmlentities'
+require 'json'
+require 'restclient'
+
+def colors
+  {
+    :reset => "\033[m",
+    :red   => "\033[1;31m",
+    :green => "\033[32m",
+    :blue  => "\033[36m"
+  }
+end
+
+def paint(color, string)
+  "#{colors[color]}#{string}#{colors[:reset]}"
+end
+
+
+boring_patterns = [
+  /^I(’m going| might go) to/,
+  /^I was there:.*http:\/\/l.songkick.com/,
+  /^Check out this (upcoming|past).*http:\/\/l.songkick.com/
+]
+html_decoder = HTMLEntities.new
+latest = nil
+
+while true
+  entries = Twitter::Search.new('songkick').since_id(latest)
+  entries.to_a.reverse.each do |entry|
+    latest = entry.id
+    
+    next if boring_patterns.any? { |pattern| entry.text =~ pattern }  
+
+    time_posted = Time.parse(entry.created_at).strftime("%a, %d-%b-%Y %T")
+    
+    text = html_decoder.decode(entry.text)
+    translator_response = JSON.parse(RestClient.get('http://ajax.googleapis.com/ajax/services/language/translate', :params => { :v=>'1.0', :q => text, :langpair => '|en' } ))["responseData"]
+    translated_text = html_decoder.decode(translator_response["translatedText"])
+    language = translator_response["detectedSourceLanguage"]
+
+
+    text.gsub!(/(@[^\s]+)/, paint(:green, '\1'))
+    
+    puts paint(:red, '@' + entry.from_user) + "\t(#{time_posted})\t#{text}"
+    puts "\t\t\t (Translated from #{language}:\t" + paint(:blue, translated_text) + ")" unless language == 'en'
+  end
+  sleep 300
+end
