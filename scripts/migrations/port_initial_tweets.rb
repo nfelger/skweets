86f5b7951e534b175ae01fb66ba12c460699994e
Niko Felger <niko.felger@gmail.com>
2011-04-08 14:50:09 +0100

    One-time migration script to port over old-format tweets

---
+++ scripts/migrations/port_initial_tweets.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'json'
+require 'redis'
+
+datastore = Redis.new
+tweets = datastore.lrange("tweets", 0, -1).map(&JSON.method(:parse))
+
+require File.dirname(__FILE__) + '/../../lib/skweets'
+
+tweets.each do |tweet|
+  Tweet.create!(
+    :id                => tweet["id"],
+    :message           => tweet["message"],
+    :profile_image_url => tweet["profile_image_url"],
+    :follower_count    => tweet["followers_count"],
+    :time_posted       => tweet["time_posted"],
+    :username          => tweet["username"]
+  )
+end
