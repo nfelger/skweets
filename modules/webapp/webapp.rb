0f7e489fe5bd056be06d38371f23c7c236dd1b1b
Niko Felger <niko.felger@gmail.com>
2011-04-13 18:06:31 +0100

    Greying out seen tweets

--- modules/webapp/webapp.rb
+++ modules/webapp/webapp.rb
@@ -5,11 +5,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../../lib/skweets.rb')
 
 class Skweets < Sinatra::Base
   set :app_file, __FILE__
-  set :static,   true # Serve static files from 'public/'.
+  set :static,   true          # Serve static files from 'public/'
 
   helpers { include ActionView::Helpers::DateHelper }
 
   get '/' do
-    erb :tweets, :locals => {:tweets => Tweet.all(:order => :id.desc, :limit => 100)}
+    erb :tweets, :locals => {
+      :tweets =>       Tweet.all(:order => :time_posted.desc, :limit => 100),
+      :last_seen_id => request.cookies["last_seen_tweet_id"].to_i
+    }
   end
 end
