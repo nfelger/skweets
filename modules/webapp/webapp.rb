6370a6531dfa2b17ac3d2e99f57a51f3e0c1a2a2
Niko Felger <niko.felger@gmail.com>
2011-04-13 15:00:22 +0100

    Limiting tweets shown

--- modules/webapp/webapp.rb
+++ modules/webapp/webapp.rb
@@ -10,6 +10,6 @@ class Skweets < Sinatra::Base
   helpers { include ActionView::Helpers::DateHelper }
 
   get '/' do
-    erb :tweets, :locals => {:tweets => Tweet.all(:order => :id.desc)}
+    erb :tweets, :locals => {:tweets => Tweet.all(:order => :id.desc, :limit => 100)}
   end
 end
